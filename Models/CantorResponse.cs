5bb06745a6024f93389110ae7f121355f189b2f4
KamilKsiazek93 <kamil.ksiazek.pp@gmail.com>
2021-09-03 13:26:16 +0200

    Added some models for apps

---
+++ Models/CantorResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDean.Models
+{
+    public class CantorResponse
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Boolean LastWeekSinging { get; set; }
+    }
+}
