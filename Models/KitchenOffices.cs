0f73b966c743d1a69ba2493bd4ac2346dc2beb38
KamilKsiazek93 <kamil.ksiazek.pp@gmail.com>
2021-09-09 09:16:22 +0200

    Adding models for litugist panel

---
+++ Models/KitchenOffices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDean.Models
+{
+    public class KitchenOffices
+    {
+        public string SaturdayOffice { get; set; }
+        public string SundayOffice { get; set; }
+    }
+}
