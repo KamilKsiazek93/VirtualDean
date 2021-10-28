2a018b1822d25af1abe7b9140b901671d7382120
KamilKsiazek93 <kamil.ksiazek.pp@gmail.com>
2021-10-28 10:50:58 +0200

    Addind meethods for getting tray hours

--- Data/TrayHour.cs
+++ Data/TrayHour.cs
@@ -24,7 +24,7 @@ namespace VirtualDean.Data
                 var brotherId = office.IdBrother;
                 var sql = "INSERT INTO trayHourOffice (userId, weekOfOffice, trayHour)" +
                     "VALUES (@brotherId, @weekOfOffice, @trayHour)";
-                foreach(VirtualDean.Enties.TrayHour hour in office.TrayHourOffices)
+                foreach(var hour in office.TrayHourOffices)
                 {
                     using var connection = new SqlConnection(_connectionString);
                     await connection.OpenAsync();
@@ -35,12 +35,44 @@ namespace VirtualDean.Data
 
         public async Task<IEnumerable<TrayOfficeAdded>> GetTrayHours()
         {
-            throw new NotImplementedException();
+            List<TrayOfficeAdded> trayOffices = new List<TrayOfficeAdded>();
+            List<String> trays = new List<String>();
+
+            using var connection = new SqlConnection(_connectionString);
+            var sqlId = "SELECT userId IdBrother FROM trayHourOffice";
+            var sqlTray = "SELECT trayHour from trayHourOffice WHERE userId = @userId";
+            await connection.OpenAsync();
+            var ids = await connection.QueryAsync<int>(sqlId);
+            foreach(var id in ids)
+            {
+                trays = (await connection.QueryAsync<String>(sqlTray, new { userId = id })).ToList();
+                if(trays.Any())
+                {
+                    trayOffices.Add(new TrayOfficeAdded() { IdBrother = id, TrayHourOffices = trays });
+                }
+            }
+            return trayOffices;
         }
 
         public async Task<IEnumerable<TrayOfficeAdded>> GetTrayHours(int weekId)
         {
-            throw new NotImplementedException();
+            List<TrayOfficeAdded> trayOffices = new List<TrayOfficeAdded>();
+            List<String> trays = new List<String>();
+
+            using var connection = new SqlConnection(_connectionString);
+            var sqlId = "SELECT userId IdBrother FROM trayHourOffice";
+            var sqlTray = "SELECT trayHour from trayHourOffice WHERE userId = @userId AND weekOfOffice = @weekId";
+            await connection.OpenAsync();
+            var ids = await connection.QueryAsync<int>(sqlId);
+            foreach (var id in ids)
+            {
+                trays = (await connection.QueryAsync<String>(sqlTray, new { userId = id, weekId = weekId })).ToList();
+                if(trays.Any())
+                {
+                    trayOffices.Add(new TrayOfficeAdded() { IdBrother = id, TrayHourOffices = trays, weekId = weekId });
+                }
+            }
+            return trayOffices;
         }
 
         private async Task<int> GetWeekNumber()
