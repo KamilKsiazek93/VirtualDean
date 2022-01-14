8922cb13705b2972076bf2adbf8f9593106ad68f
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-01-14 14:56:40 +0100

    Refactor obstacles methods with Entity Framework

---
+++ Models/DatabaseContext/ObstaclesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDean.Models.DatabaseContext
+{
+    public class ObstaclesDbContext : DbContext
+    {
+        public DbSet<ObstaclesAdded> Obstacles { get; set; }
+        public ObstaclesDbContext(DbContextOptions<ObstaclesDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ObstaclesAdded>().ToTable("Obstacles");
+        }
+    }
+}
--- Models/ObstaclesList.cs
+++ Models/ObstaclesList.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace VirtualDean.Models
 {
-    public class ObstaclesDb : BaseModel
+    public class ObstaclesList
     {
+        public int BrotherId { get; set; }
         public List<string> Obstacles { get; set; }
     }
 }
