637b7bbed88ff25ee40cb0d6e7740bd27f608bf0
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-05-09 11:43:52 +0200

    Change communion hour class definition. Added dbContext

--- Data/TrayCommunionHour.cs
+++ Data/TrayCommunionHour.cs
@@ -23,27 +23,9 @@ namespace VirtualDean.Data
             _week = week;
         }
 
-        public async Task AddSingleCommunionHour(int brotherId, int weekNumber, string hour)
-        {
-            var sql = "INSERT INTO communionHourOffice (userId, weekOfOffices, communionHour)" +
-                "VALUES (@brotherId, @weekOfOffice, @communionHour)";
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            await connection.ExecuteAsync(sql, new { brotherId = brotherId, weekOfOffice = weekNumber, communionHour = hour });
-
-        }
-
         public async Task AddCommunionHour(IEnumerable<CommunionOfficeAdded> offices)
         {
-            int weekNumber = await _week.GetLastWeek();
-            foreach (CommunionOfficeAdded office in offices)
-            {
-                var brotherId = office.IdBrother;
-                foreach (var hour in office.CommunionHourOffices)
-                {
-                    await AddSingleCommunionHour(brotherId, weekNumber, hour);
-                }
-            }
+            
         }
 
         public async Task AddTrayHour(IEnumerable<TrayOfficeAdded> offices)
@@ -60,38 +42,12 @@ namespace VirtualDean.Data
 
         public async Task<IEnumerable<CommunionOfficeAdded>> GetCommunionHours()
         {
-            List<CommunionOfficeAdded> communionOffice = new List<CommunionOfficeAdded>();
-            List<String> communionHour = new List<String>();
-
-            using var connection = new SqlConnection(_connectionString);
-            var sql = "SELECT DISTINCT userId IdBrother, communionHour OfficeName FROM communionHourOffice";
-            await connection.OpenAsync();
-            var result = (await connection.QueryAsync<SingleOfficeWithID>(sql)).ToList();
-            var ids = result.Select(item => item.IdBrother).Distinct();
-            foreach (var id in ids)
-            {
-                communionHour = (result.Where(item => item.IdBrother == id).Select(item => item.OfficeName)).ToList();
-                communionOffice.Add(new CommunionOfficeAdded() { IdBrother = id, CommunionHourOffices = communionHour });
-            }
-            return communionOffice;
+            
         }
 
         public async Task<IEnumerable<CommunionOfficeAdded>> GetCommunionHours(int weekId)
         {
-            List<CommunionOfficeAdded> communionOffice = new List<CommunionOfficeAdded>();
-            List<String> communionHour = new List<String>();
-
-            using var connection = new SqlConnection(_connectionString);
-            var sql = "SELECT userId IdBrother, communionHour OfficeName FROM communionHourOffice WHERE weekOfOffices = @weekId";
-            await connection.OpenAsync();
-            var result = (await connection.QueryAsync<SingleOfficeWithID>(sql, new { weekId })).ToList();
-            var ids = result.Select(item => item.IdBrother).Distinct();
-            foreach (var id in ids)
-            {
-                communionHour = (result.Where(item => item.IdBrother == id).Select(item => item.OfficeName)).ToList();
-                communionOffice.Add(new CommunionOfficeAdded() { IdBrother = id, CommunionHourOffices = communionHour, weekId = weekId });
-            }
-            return communionOffice;
+            
         }
         public async Task<IEnumerable<TrayOfficeAdded>> GetTrayHours()
         {
---
+++ Models/DatabaseContext/CommunionHourDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDean.Models.DatabaseContext
+{
+    public class CommunionHourDbContext : DbContext
+    {
+        public DbSet<CommunionOfficeAdded> CommunionHourOffice { get; set; }
+        public CommunionHourDbContext(DbContextOptions<CommunionHourDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
