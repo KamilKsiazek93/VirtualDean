911425680d0a3d97c1c244b60bec4f22fc760cc6
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-06-07 19:43:22 +0200

    Added const for tray and communion hours

---
+++ Enties/Hours.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VirtualDean.Enties
+{
+    public static class Hours
+    {
+        public const string T8 = "8.00";
+        public const string T9 = "9.00";
+        public const string T10 = "10.30";
+        public const string T12 = "12.00";
+        public const string T13 = "13.30";
+        public const string T15 = "15.30";
+        public const string T17 = "17.00";
+        public const string T19 = "19.00";
+        public const string T20 = "20.20";
+        public const string T21 = "21.30";
+
+        public const string COMMUNION_HOUR_TO_EXCLUDE = "10.30";
+    }
+}
