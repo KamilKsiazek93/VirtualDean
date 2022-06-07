d00004702f7aea1d2f0041d268282e43a620e6ad
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-06-07 20:55:39 +0200

    Added method for validate tray and communion hours

--- Enties/Hours.cs
+++ Enties/Hours.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 
 namespace VirtualDean.Enties
 {
-    public static class Hours
+    public class Hours
     {
-        public const string T8 = "8.00";
-        public const string T9 = "9.00";
-        public const string T10 = "10.30";
-        public const string T12 = "12.00";
-        public const string T13 = "13.30";
-        public const string T15 = "15.30";
-        public const string T17 = "17.00";
-        public const string T19 = "19.00";
-        public const string T20 = "20.20";
-        public const string T21 = "21.30";
+        private const string T8 = "8.00";
+        private const string T9 = "9.00";
+        private const string T10 = "10.30";
+        private const string T12 = "12.00";
+        private const string T13 = "13.30";
+        private const string T15 = "15.30";
+        private const string T17 = "17.00";
+        private const string T19 = "19.00";
+        private const string T20 = "20.20";
+        private const string T21 = "21.30";
 
-        public const string COMMUNION_HOUR_TO_EXCLUDE = "10.30";
+        private const string COMMUNION_HOUR_TO_EXCLUDE = "10.30";
+
+        public IEnumerable<string> TrayHours { get { return new List<string>() { T8, T9, T10, T12, T13, T15, 
+            T17, T19, T20, T21 }; 
+            } }
+
+        public IEnumerable<string> CommunionHours { get {
+                return TrayHours.Where(item => item != COMMUNION_HOUR_TO_EXCLUDE);
+            } }
     }
 }
