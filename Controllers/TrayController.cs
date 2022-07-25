1020661cbe3c4055cdc07c37e7dbe97c45fd2e24
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-07-25 20:33:24 +0200

    Implement method for tray controller

--- Controllers/TrayController.cs
+++ Controllers/TrayController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using VirtualDean.Data;
+using VirtualDean.Enties;
+using VirtualDean.Models;
 
 namespace VirtualDean.Controllers
 {
@@ -13,13 +17,67 @@ namespace VirtualDean.Controllers
     [ApiController]
     public class TrayController : ControllerBase
     {
-        private readonly ITrayCommunionHour _trayCommunionHour;
+        private readonly ITrayRepository _trayRepository;
         private readonly IHttpClientFactory _clientFactory;
 
-        public TrayController(ITrayCommunionHour trayCommunionHour, IHttpClientFactory clientFactory)
+        public TrayController(ITrayRepository trayRepository, IHttpClientFactory clientFactory)
         {
-            _trayCommunionHour = trayCommunionHour;
+            _trayRepository = trayRepository;
             _clientFactory = clientFactory;
+        }
+
+        [Authorize(Policy = "Liturgist")]
+        [HttpPost("tray-hour")]
+        public async Task<ActionResult> AddTrayOffice(IEnumerable<TrayOfficeAdded> listOfTray)
+        {
+            try
+            {
+                var client = _clientFactory.CreateClient("Offices");
+                if (!await IsOfficeAvailableToSet(PipelineConstName.TRAY))
+                {
+                    return NotFound(new { message = ActionResultMessage.OfficeNotAdded });
+                }
+                await _trayRepository.AddTrayHour(listOfTray);
+                await client.PutAsJsonAsync("pipeline", new PipelineUpdate { JobName = PipelineConstName.TRAY, JobValue = false });
+                await client.PutAsJsonAsync("pipeline", new PipelineUpdate { JobName = PipelineConstName.LITURGIST, JobValue = true });
+                return Ok(new { message = ActionResultMessage.OfficeAdded });
+            }
+            catch
+            {
+                return NotFound(new { message = ActionResultMessage.OperationFailed });
+            }
+        }
+
+        [HttpGet("tray-hour")]
+        public async Task<IEnumerable<TrayOfficeAdded>> GetTrayHour()
+        {
+            return await _trayRepository.GetTrayHours();
+        }
+
+        [HttpGet("tray-hour/{weekId}")]
+        public async Task<IEnumerable<TrayOfficeAdded>> GetTrayHour(int weekId)
+        {
+            return await _trayRepository.GetTrayHours(weekId);
+        }
+
+        [HttpGet("tray-hour-last")]
+        public async Task<IEnumerable<LastTrayOfficeList>> GetLastTray()
+        {
+            return await _trayRepository.GetLastTrayHour();
+        }
+
+        [HttpGet("tray-hour-last/{brotherId}")]
+        public async Task<IEnumerable<string>> GetLastTrayForBrother(int brotherId)
+        {
+            var client = _clientFactory.CreateClient("Weeks");
+            int weekNumber = await client.GetFromJsonAsync<int>("");
+            return await _trayRepository.GetTrayHour(weekNumber, brotherId);
+        }
+
+        private Task<bool> IsOfficeAvailableToSet(string officeName)
+        {
+            var client = _clientFactory.CreateClient("Offices");
+            return client.GetFromJsonAsync<bool>($"pipeline-status/{officeName}");
         }
     }
 }
